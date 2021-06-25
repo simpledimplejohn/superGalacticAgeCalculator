d08afdbe0052320dd24c7da9bee7e6b0ed7358b3
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-25 16:05:23 -0700

    update test one name

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -2,7 +2,7 @@ import UserAge from './../src/js/scripts.js'
 
 describe('UserAge', () => {
 
-  test('should be able to find UserAge class', () => {
+  test('should correctly create a user object with three properties', () => {
 
     const newUserAge = new UserAge("john", 42, 74);
     expect(newUserAge.userName).toEqual("john")
