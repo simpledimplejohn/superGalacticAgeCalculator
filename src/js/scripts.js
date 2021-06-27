17c48dab5f4fd454013412d204870f9225b13af9
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-27 13:09:09 -0700

    add newLifeExpect() to UserAge

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,13 +3,38 @@ console.log("scripts!");
 
 
 export default class UserAge {
-  constructor (userName, age, lifeExpect, planet) {
+  constructor (userName, age, lifeExpect, planet, habitat) {
     this.userName = userName;
     this.age = age;
     this.lifeExpect = lifeExpect;
     this.planet = planet
+    this.habitat = habitat
+  }
+  newPlanetAge () {
+    if (this.planet === "mercury") {
+      return this.age * (365/88);
+    } else if (this.planet === "venus") {
+      return this.age * (365/225);
+    } else if (this.planet === "mars") {
+      return this.age * (365/687);
+    } else if (this.planet === "jupiter") {
+      return this.age * (365/4300);
+    } else {
+      return "no data"
+    }
   }
 
+  newLifeExpect () {
+    if (this.habitat === "spaceSuit") {
+      return this.lifeExpect / 50;
+    } else if (this.habitat === "outpost") {
+      return this.lifeExpect / 10;
+    } else if (this.habitat === "advanced civilization") {
+      return this.lifeExpect * 10;
+    } else {
+      return this.lifeExpect / 1000;
+    }
+  }
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import UserAge from './../src/js/scripts.js'
 
 console.log("main!")
-const newUserAge = new UserAge ("john", 42, 74, "mercury");
+const newUserAge = new UserAge ("john", 42, 74, "jupiter", "advanced civilization");
+
+
 
 // UI interface through the console
 
@@ -9,3 +11,5 @@ console.log("For the user named: ", newUserAge.userName);
 
 console.log('On Earth this person is: ', newUserAge.age, "years old");
 console.log('on earth a person could be expected to live to be ', newUserAge.lifeExpect)
+console.log('if you lived on', newUserAge.planet,'you would be ', Math.floor(newUserAge.newPlanetAge()), " years old in ", newUserAge.planet, " years");
+console.log('you should be expected to live to be ', newUserAge.newLifeExpect(), "years oldliving on", newUserAge.planet, "with a", newUserAge.habitat, "for protection");
