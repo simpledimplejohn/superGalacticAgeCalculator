09018cfe25efda8ffd765373ce607ae8e9e951ff
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-30 04:26:11 -0700

    update first test

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -6,6 +6,10 @@ describe('UserAge', () => {
 
     const newUserAge = new UserAge("john", 42, 74, "mercury", "outpost");
     expect(newUserAge.userName).toEqual("john")
+    expect(newUserAge.age).toEqual(43)
+    expect(newUserAge.lifeExpect).toEqual(74)
+    expect(newUserAge.planet).toEqual("mercury")
+    expect(newUserAge.habitat).toEqual("outpost")
   });
 
 });
