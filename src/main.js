0417223cb7dc6555a841c0bb68fd8c68aab361fa
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-05 15:31:11 -0700

    calculate user life left

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import UserAge from './../src/js/scripts.js'
 
-const newUserAge = new UserAge ("john", 20, 14, "mercury", "outpost");
+const newUserAge = new UserAge ("john", 42, 75, "mercury", "outpost");
 
 // UI interface through the console
 
@@ -10,7 +10,9 @@ console.log('Find out how long you will live in space');
 console.log("For the user named: ", newUserAge.userName);
 console.log('On Earth this person is: ', newUserAge.age, "years old");
 console.log('on earth a person could be expected to live to be ', newUserAge.lifeExpect);
-console.log('You have exceeded your life expectancy by: ', newUserAge.lifeExpect, newUserAge.lifeExpect - newUserAge.age)
+
 console.log('if you lived on', newUserAge.planet,'you would be ', Math.floor(newUserAge.newPlanetAge()), " years old in ", newUserAge.planet, " years");
-console.log('you should be expected to live to be ', newUserAge.newLifeExpect(), "years old living on", newUserAge.planet, "with a", newUserAge.habitat, "for protection");
+console.log('you should be expected to live ', newUserAge.newLifeExpect(), "more years on ", newUserAge.planet, "with a", newUserAge.habitat, "for protection");
+console.log('that means you will live to be:', newUserAge.age + newUserAge.newLifeExpect())
+
 console.log("just as an aside ", newUserAge.userName, ", ", newUserAge.howOld());
