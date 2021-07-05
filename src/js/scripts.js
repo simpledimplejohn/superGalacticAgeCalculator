adc141f533abd3b9ea04b653d045d778ba1e6740
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-05 16:01:45 -0700

    pass second test

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,9 @@
 export default class UserAge {
-  
+  constructor (userName, age, lifeExpect, planet, habitat) {
+    this.userName = userName;
+    this.age = age;
+    this.lifeExpect = lifeExpect;
+    this.planet = planet;
+    this.habitat = habitat;
+  }
 }
