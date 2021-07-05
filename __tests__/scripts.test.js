dcfe37dd5b073ca10de33e30801bfcec37a150c4
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-05 15:55:52 -0700

    fail first test, UserAge not present

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -1,2 +1,10 @@
 import UserAge from './../src/js/scripts.js'
 
+describe('UserAge', () => {
+
+  test('should register that the class UserAge is present', () => {
+
+    const newUserAge = new UserAge();
+
+  });
+})
