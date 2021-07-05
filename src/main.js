ae60e3c0eab97c43c962f12a110d038d1fb59a8f
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-05 09:00:09 -0700

    wip lifeExpect

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ console.log('Find out how long you will live in space');
 console.log("For the user named: ", newUserAge.userName);
 console.log('On Earth this person is: ', newUserAge.age, "years old");
 console.log('on earth a person could be expected to live to be ', newUserAge.lifeExpect);
-console.log('You have exceeded your life expectancy by: ')
+console.log('You have exceeded your life expectancy by: ', newUserAge.lifeExpect)
 console.log('if you lived on', newUserAge.planet,'you would be ', Math.floor(newUserAge.newPlanetAge()), " years old in ", newUserAge.planet, " years");
 console.log('you should be expected to live to be ', newUserAge.newLifeExpect(), "years old living on", newUserAge.planet, "with a", newUserAge.habitat, "for protection");
 console.log("just as an aside ", newUserAge.userName, ", ", newUserAge.howOld());
