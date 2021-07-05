821efdb47887fa9cc52b75b37faf522924f83244
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-05 16:00:54 -0700

    fail second test, newUserAge with content

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -3,8 +3,17 @@ import UserAge from './../src/js/scripts.js'
 describe('UserAge', () => {
 
   test('should register that the class UserAge is present', () => {
-
     const newUserAge = new UserAge();
-
   });
+
+  test('should correctly create a user object with five properties', () => {
+
+    const newUserAge = new UserAge("john", 42, 74, "mercury", "outpost");
+    expect(newUserAge.userName).toEqual("john")
+    expect(newUserAge.age).toEqual(42)
+    expect(newUserAge.lifeExpect).toEqual(74)
+    expect(newUserAge.planet).toEqual("mercury")
+    expect(newUserAge.habitat).toEqual("outpost")
+  });
+
 })
